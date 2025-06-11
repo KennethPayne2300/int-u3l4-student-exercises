fd402041493eea0ac0485b748ea2a9d33c2eade3
KennethPayne2300 <kennethpayne353@gmail.com>
2025-06-11 13:40:48 -0500

    finished 3.4

--- domManipulation.js
+++ domManipulation.js
@@ -22,24 +22,28 @@ let image = document.querySelector('.image');
 //   - The `location` paragraph.
 
 contentButton.addEventListener('click', function() {
-
-
-
-
-
-
+    myName.innerHTML = "My Name is Kenneth";
+    myHobby.innerHTML = "I like to Play video games";
+    myLocation.innerHTML = "I live in evansville";
 });
 
 // 3. Add an event listener to the `image1Button`
 // 4. When clicked, it should change the source of `image` to another image URL.
-
+image1Button.addEventListener('click', function() {
+    image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNJ3rXPwLyaYzM7fzE5ggUE32_xV3WPKIGPg&s";
+});
 
 
 
 
 // 5. Add an event listener to the `image2Button`
 // 6. When clicked, it should change the source of `image` to another image URL.
-
+image2Button.addEventListener('click', function() {
+    image.src = "https://upload.wikimedia.org/wikipedia/en/1/19/Marvel_Universe_%28Civil_War%29.jpg";
+});
 
 // BONUS
 // 7. Try using different event types for your buttons.
+image1Button.addEventListener('dblclick', function(){
+    image.src = "https://m.media-amazon.com/images/M/MV5BNmMyM2RkNDEtYzI0MS00ZGM3LWI1YzYtY2Y0YzEzMGY3NTlmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg";
+});
